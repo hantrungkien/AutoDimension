d9684f76374fdb648259c2bab9cacc5fb1e31ac5
KienHT <kienhantrung@gmail.com>
2020-10-08 21:53:19 +0700

    update v1.0.5

--- app/src/main/java/htkien/autodimension/MainActivity.java
+++ app/src/main/java/htkien/autodimension/MainActivity.java
@@ -1,7 +1,9 @@
 package htkien.autodimension;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_dpi);
+        String dimenFileName = getString(R.string.dimenFileName);
+        textView.setText(dimenFileName);
     }
 }
